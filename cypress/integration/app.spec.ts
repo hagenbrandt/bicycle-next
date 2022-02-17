84a5661db452693b35f78b1fa620835f6e6606e1
Hagen Brandt <h.brandt@steinberg.de>
2022-02-17 09:14:55 +0100

    test: Create basic example test for Home with cypress

---
+++ cypress/integration/app.spec.ts
@@ -0,0 +1,8 @@
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('a[href*="about"]').click()
+    cy.url().should('include', '/about')
+    cy.get('h1').contains('About Page')
+  })
+})
