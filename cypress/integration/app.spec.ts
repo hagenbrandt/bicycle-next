91207481edc0fb36dafbde7b2294e32c659d7e6b
Hagen Brandt <h.brandt@steinberg.de>
2022-02-17 13:17:26 +0100

    build: Set baseUrl and viewport to cypress.json

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,6 +1,6 @@
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
     cy.get('a[href*="about"]').click()
     cy.url().should('include', '/about')
     cy.get('h1').contains('About Page')
