59c9494255fcb7583feb2855b322910743806b53
Hagen Brandt <h.brandt@steinberg.de>
2022-03-11 10:00:19 +0100

    build: Install @washingtonpost/storybook-addon-web-vitals

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     '@storybook/addon-storysource',
+    '@washingtonpost/storybook-addon-web-vitals',
   ],
   framework: '@storybook/react',
   core: {
