274ed710cf303c365b396c4e552f03b559e670ae
Hagen Brandt <h.brandt@steinberg.de>
2022-06-22 11:24:34 +0200

    build: Delete cypress.json and use cypress.config.ts instead

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+    viewportHeight: 800,
+    viewportWidth: 1280,
+    video: false,
+    testFiles: ['**/*.spec.ts', '**/*.feature'],
+    ignoreTestFiles: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
+  },
+})
