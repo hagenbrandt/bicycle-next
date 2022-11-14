beaa19bb54406731951254d865f3124462748f8d
hbrandt <h.brandt@steinberg.de>
2022-11-14 16:08:25 +0100

    feat: Render links in navigation

--- __tests__/components/Navigation.test.tsx
+++ __tests__/components/Navigation.test.tsx
@@ -1,13 +1,41 @@
 import { render, screen } from '@testing-library/react'
-import Navigation from '../../components/navigation/Navigation'
+import Navigation, { LinkParams } from '../../components/navigation/Navigation'
 
 describe('Navigation', () => {
-  it('renders a navigation element', () => {
-    render(<Navigation />)
+  const links: LinkParams[] = [
+    {
+      linkText: 'About',
+      linkHref: '/about',
+    },
+    {
+      linkText: 'Page 1',
+      linkHref: '/page-one',
+    },
+    {
+      linkText: 'Page 2',
+      linkHref: '/page-two',
+    },
+  ]
 
+  beforeEach(() => {
+    render(<Navigation links={links} />)
+  })
+
+  it('renders a navigation element', () => {
     expect(screen.getByRole('navigation')).toBeInTheDocument()
   })
 
-  it.todo('renders given links with strings and hrefs')
+  it('renders given links with strings and hrefs', () => {
+    links.forEach((item) => {
+      expect(
+        screen.getByRole('link', { name: item.linkText })
+      ).toHaveTextContent(item.linkText)
+      expect(screen.getByRole('link', { name: item.linkText })).toHaveAttribute(
+        'href',
+        item.linkHref
+      )
+    })
+  })
+  it.todo('returns empty DOM element when no links are given')
   it.todo('navigates to passed href url')
 })
--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,5 +1,27 @@
-const Navigation = () => {
-  return <nav></nav>
+import Link from 'next/link'
+
+export type LinkParams = {
+  linkText: string
+  linkHref: string
+}
+
+type NavigationProps = {
+  links: LinkParams[]
+}
+
+const Navigation = ({ links }: NavigationProps) => {
+  return (
+    <nav>
+      {links.map((item, index) => {
+        return (
+          <Link href={item.linkHref} key={item.linkText + index}>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a>{item.linkText}</a>
+          </Link>
+        )
+      })}
+    </nav>
+  )
 }
 
 export default Navigation
